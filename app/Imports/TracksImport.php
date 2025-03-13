7d53a752decf4fff581efc2e5e7903cbc364c65e
FateevMaxim <fateev.maxim@gmail.com>
2025-03-13 12:51:31 +0500

    new branch

--- app/Imports/TracksImport.php
+++ app/Imports/TracksImport.php
@@ -6,39 +6,57 @@ use App\Models\TrackList;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\SkipsOnError;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class TracksImport implements ToModel, SkipsOnError
+class TracksImport implements ToModel, SkipsOnError, WithChunkReading
 {
-
     use Importable;
+
     private $date;
 
     public function __construct(string $date)
     {
         $this->date = $date;
     }
+
     /**
-     * @param \Throwable $e
+     * Handle rows from the imported file.
+     *
+     * @param array $row
+     * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function onError(\Throwable $e)
-    {
-        // Handle the exception how you'd like.
-    }
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
     public function model(array $row)
     {
-        if ($row[1] && $row[1] != '') {
+        if (!empty($row[1])) {
             return new TrackList([
                 'track_code' => $row[1],
                 'to_china' => $this->date,
                 'status' => 'Получено в Китае',
                 'reg_china' => 1,
-                'created_at' => date(now()),
+                'created_at' => now(),
             ]);
         }
+
+        return null;
+    }
+
+    /**
+     * Handle errors during the import process.
+     *
+     * @param \Throwable $e
+     */
+    public function onError(\Throwable $e)
+    {
+        // Log or handle exceptions.
+    }
+
+    /**
+     * Define the size of the chunks to read.
+     *
+     * @return int
+     */
+    public function chunkSize(): int
+    {
+        return 30; // Number of rows to process per chunk
     }
 }
